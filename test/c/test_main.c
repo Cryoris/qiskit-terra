35d83eac50e07eab00b94bd4fa6f094e71968b5b
Julien Gacon <jules.gacon@googlemail.com>
2024-12-05 17:41:49 +0100

    test for linux

--- test/c/test_main.c
+++ test/c/test_main.c
@@ -2,12 +2,13 @@
 
 int main()
 {
-    // SparseObservable *obs = obs_zero(100);
-    // obs_print(obs);
-    // obs_deallocate(obs);
+    SparseObservable *obs = obs_zero(100);
+    obs_print(obs);
+    obs_deallocate(obs);
     // BitTerm *bit = bit_term(uin1);
     // bit_term_deallocate(bit);
-    enum BitTerm bit;
-    bit = Y;
+    // enum BitTerm bit;
+    // bit = Y;
+
     return 0;
 }
