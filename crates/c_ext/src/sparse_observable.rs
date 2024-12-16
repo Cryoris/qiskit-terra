1bb45b91c7bed34ae0627219af79689aaaf27d1e
Julien Gacon <jules.gacon@googlemail.com>
2024-12-16 13:56:35 +0100

    enable C API
    and add some tests

--- crates/c_ext/src/sparse_observable.rs
+++ crates/c_ext/src/sparse_observable.rs
@@ -10,13 +10,8 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
+use num_complex::Complex64;
 use qiskit_accelerate::sparse_observable::SparseObservable;
-
-#[no_mangle]
-#[cfg(feature = "cbinding")]
-pub extern "C" fn hello() {
-    println!("Sparse observable will go here!");
-}
 
 #[no_mangle]
 #[cfg(feature = "cbinding")]
@@ -27,14 +22,125 @@ pub extern "C" fn obs_zero(num_qubits: u32) -> *mut SparseObservable {
 
 #[no_mangle]
 #[cfg(feature = "cbinding")]
+pub extern "C" fn obs_identity(num_qubits: u32) -> *mut SparseObservable {
+    let obs = SparseObservable::identity(num_qubits);
+    Box::into_raw(Box::new(obs))
+}
+
+/// Multiply an observable by a complex coefficient.
+///
+/// Example:
+///     
+///     SparseObservable* obs = obs_identity(100);
+///     SparseObservable* result = obs_multiply(obs, 2);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_multiply(
+    observable: &SparseObservable,
+    coeff: Complex64,
+) -> *mut SparseObservable {
+    let result = observable * coeff;
+    Box::into_raw(Box::new(result))
+}
+
+/// Add two observables.
+///
+/// Example:
+///     
+///     SparseObservable* left = obs_identity(100);
+///     SparseObservable* right = obs_zero(100);
+///     SparseObservable* result = obs_add(left, right);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_add(
+    left: &SparseObservable,
+    right: &SparseObservable,
+) -> *mut SparseObservable {
+    let result = left + right;
+    Box::into_raw(Box::new(result))
+}
+
+/// Calculate the canonical representation of the observable.
+///
+/// Example:
+///
+///     SparseObservable* iden = obs_identity(100);
+///     SparseObservable* two = obs_add(iden, iden);
+///
+///     let double tol = 1e-6;
+///     SparseObservable* canonical = obs_canonicalize(two);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_canonicalize(
+    observable: &SparseObservable,
+    tol: f64, // no optional arguments in C -- welcome to the ancient past
+) -> *mut SparseObservable {
+    let result = observable.canonicalize(tol);
+    Box::into_raw(Box::new(result))
+}
+
+/// Copy the observable.
+///
+/// Example:
+///
+///     SparseObservable* original = obs_identity(100);
+///     SparseObservable* copied = obs_copy(original);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_copy(observable: &SparseObservable) -> *mut SparseObservable {
+    let copied = observable.clone();
+    Box::into_raw(Box::new(copied))
+}
+
+/// Deallocate the observable.
+///
+/// Memory deallocation is user responsibility. Every constructed observable
+/// must be deallocated manually to avoid memory leakage.
+///
+/// Example:
+///
+///     SparseObservable* obs = obs_zero(100);
+///     obs_deallocate(obs);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
 pub extern "C" fn obs_deallocate(observable: &mut SparseObservable) {
     unsafe {
         let _ = Box::from_raw(observable);
     }
 }
 
+/// Get the number of terms in the observable.
+///
+/// Example:
+///
+///     SparseObservable* obs = obs_identity(100);
+///     int num_terms = obs_num_terms(obs);  // 1
 #[no_mangle]
 #[cfg(feature = "cbinding")]
-pub extern "C" fn obs_print(observable: &mut SparseObservable) {
+pub extern "C" fn obs_num_terms(observable: &SparseObservable) -> u64 {
+    observable.num_terms() as u64
+}
+
+/// Get the number of qubits the observable is defined on.
+///
+/// Example:
+///
+///     SparseObservable* obs = obs_identity(100);
+///     int num_qubits = obs_num_qubits(obs);  // 100
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_num_qubits(observable: &SparseObservable) -> u32 {
+    observable.num_qubits() as u32
+}
+
+/// Print the observable.
+///
+/// Example:
+///
+///     SparseObservable* obs = obs_identity(100);
+///     obs_print(obs);
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub extern "C" fn obs_print(observable: &SparseObservable) {
     println!("{:?}", observable);
 }
--- test/c/test_main.c
+++ test/c/test_main.c
@@ -1,17 +1,158 @@
 #include <stdio.h>
+#include <complex.h>
 #include "qiskit.h"
+
+int test_zero()
+{
+    printf("\n-- test_zero\n");
+    SparseObservable *obs = obs_zero(100);
+    obs_print(obs);
+    obs_deallocate(obs);
+    return 0;
+}
+
+int test_identity()
+{
+    printf("\n-- test_identity\n");
+    SparseObservable *obs = obs_identity(100);
+    obs_print(obs);
+    obs_deallocate(obs);
+    return 0;
+}
+
+int test_copy()
+{
+    printf("\n-- test_copy\n");
+    SparseObservable *obs = obs_identity(100);
+    SparseObservable *copied = obs_copy(obs);
+    obs_print(copied);
+    obs_deallocate(obs);
+    obs_deallocate(copied);
+    return 0;
+}
+
+int test_add()
+{
+    printf("\n-- test_add\n");
+    SparseObservable *left = obs_identity(100);
+    SparseObservable *right = obs_identity(100);
+    SparseObservable *obs = obs_add(left, right);
+
+    obs_print(obs);
+
+    obs_deallocate(left);
+    obs_deallocate(right);
+    obs_deallocate(obs);
+
+    return 0;
+}
+
+int test_mult_real()
+{
+    printf("\n-- test_mult_real\n");
+    SparseObservable *obs = obs_identity(100);
+
+    double coeff = 2;
+    SparseObservable *result = obs_multiply(obs, coeff);
+
+    obs_print(result);
+
+    obs_deallocate(obs);
+    obs_deallocate(result);
+
+    return 0;
+}
+
+int test_mult_complex()
+{
+    printf("\n-- test_mult_complex\n");
+    SparseObservable *obs = obs_identity(100);
+
+    complex double coeff = 2 + 2 * I;
+    SparseObservable *result = obs_multiply(obs, coeff);
+
+    obs_print(result);
+
+    obs_deallocate(obs);
+    obs_deallocate(result);
+
+    return 0;
+}
+
+int test_canonicalize()
+{
+    printf("\n-- test_canonicalize\n");
+    SparseObservable *left = obs_identity(100);
+    SparseObservable *right = obs_identity(100);
+    SparseObservable *obs = obs_add(left, right);
+
+    double tol = 1e-5;
+    SparseObservable *simplified = obs_canonicalize(obs, tol);
+
+    obs_print(simplified);
+
+    obs_deallocate(obs);
+    obs_deallocate(right);
+    obs_deallocate(left);
+    obs_deallocate(simplified);
+
+    return 0;
+}
+
+int test_num_terms()
+{
+    printf("\n-- test_num_terms\n");
+
+    uint64_t num_terms;
+
+    SparseObservable *zero = obs_zero(100);
+    num_terms = obs_num_terms(zero);
+    printf("zero: %llu\n", num_terms);
+    obs_deallocate(zero);
+
+    SparseObservable *iden = obs_identity(100);
+    num_terms = obs_num_terms(iden);
+    printf("identity: %llu\n", num_terms);
+    obs_deallocate(iden);
+
+    return 0;
+}
+
+int test_num_qubits()
+{
+    printf("\n-- test_num_qubits\n");
+
+    uint32_t num_qubits;
+
+    SparseObservable *obs = obs_zero(1);
+    num_qubits = obs_num_qubits(obs);
+    printf("1 qubit: %u\n", num_qubits);
+    obs_deallocate(obs);
+
+    SparseObservable *obs100 = obs_zero(100);
+    num_qubits = obs_num_qubits(obs100);
+    printf("100 qubits: %u\n", num_qubits);
+    obs_deallocate(obs100);
+
+    return 0;
+}
 
 int main()
 {
-    // SparseObservable *obs = obs_zero(100);
-    // obs_print(obs);
-    // obs_deallocate(obs);
-    BitTerm *bit = bit_term(1);
-    printf("%i", *bit);
-    bit_term_print(bit);
-    bit_term_deallocate(bit);
-    // enum BitTerm bit;
-    // bit = Y;
+    // BitTerm *bit = bit_term(1);
+    // printf("%i", *bit);
+    // bit_term_print(bit);
+    // bit_term_deallocate(bit);
+
+    test_zero();
+    test_identity();
+    test_add();
+    test_mult_real();
+    test_mult_complex();
+    test_canonicalize();
+    test_copy();
+    test_num_terms();
+    test_num_qubits();
 
     return 0;
 }
