a8fdabb06fe6750c39d4ecfb32d64f448253cc14
Max Rossmannek <oss@zurich.ibm.com>
2024-12-18 15:10:59 +0100

    Some actual test assertions

--- test/c/test_sparse_observable.c
+++ test/c/test_sparse_observable.c
@@ -2,10 +2,16 @@
 #include <complex.h>
 #include "qiskit.h"
 
+enum TestResult {
+    Ok,
+    EqualityError,
+};
+
 int test_zero()
 {
     SparseObservable *obs = obs_zero(100);
     obs_print(obs);
+    // TODO: actually perform some equality check (requires obs_term)
     obs_deallocate(obs);
     return 0;
 }
@@ -14,6 +20,7 @@ int test_identity()
 {
     SparseObservable *obs = obs_identity(100);
     obs_print(obs);
+    // TODO: actually perform some equality check (requires obs_term)
     obs_deallocate(obs);
     return 0;
 }
@@ -23,6 +30,7 @@ int test_copy()
     SparseObservable *obs = obs_identity(100);
     SparseObservable *copied = obs_copy(obs);
     obs_print(copied);
+    // TODO: actually perform some equality check (requires obs_term)
     obs_deallocate(obs);
     obs_deallocate(copied);
     return 0;
@@ -35,6 +43,7 @@ int test_add()
     SparseObservable *obs = obs_add(left, right);
 
     obs_print(obs);
+    // TODO: actually perform some equality check (requires obs_term)
 
     obs_deallocate(left);
     obs_deallocate(right);
@@ -51,6 +60,7 @@ int test_mult_real()
     SparseObservable *result = obs_multiply(obs, coeff);
 
     obs_print(result);
+    // TODO: actually perform some equality check (requires obs_term)
 
     obs_deallocate(obs);
     obs_deallocate(result);
@@ -66,6 +76,7 @@ int test_mult_complex()
     SparseObservable *result = obs_multiply(obs, coeff);
 
     obs_print(result);
+    // TODO: actually perform some equality check (requires obs_term)
 
     obs_deallocate(obs);
     obs_deallocate(result);
@@ -83,6 +94,7 @@ int test_canonicalize()
     SparseObservable *simplified = obs_canonicalize(obs, tol);
 
     obs_print(simplified);
+    // TODO: actually perform some equality check (requires obs_term)
 
     obs_deallocate(obs);
     obs_deallocate(right);
@@ -94,40 +106,64 @@ int test_canonicalize()
 
 int test_num_terms()
 {
+    int result = Ok;
     uint64_t num_terms;
 
     SparseObservable *zero = obs_zero(100);
     num_terms = obs_num_terms(zero);
+    if (num_terms != 0)
+    {
+        result = EqualityError;
+    }
     obs_deallocate(zero);
 
     SparseObservable *iden = obs_identity(100);
     num_terms = obs_num_terms(iden);
+    if (num_terms != 1)
+    {
+        result = EqualityError;
+    }
     obs_deallocate(iden);
 
-    return 0;
+    return result;
 }
 
 int test_num_qubits()
 {
+    int result = Ok;
     uint32_t num_qubits;
 
     SparseObservable *obs = obs_zero(1);
     num_qubits = obs_num_qubits(obs);
+    if (num_qubits != 1)
+    {
+        result = EqualityError;
+    }
     obs_deallocate(obs);
 
     SparseObservable *obs100 = obs_zero(100);
     num_qubits = obs_num_qubits(obs100);
+    if (num_qubits != 100)
+    {
+        result = EqualityError;
+    }
     obs_deallocate(obs100);
 
-    return 0;
+    return result;
 }
 
 int run(char* name, int result)
 {
-    int did_fail = 0;
-    char* msg = "OK";
-    if (result > 0)
+    int did_fail = 1;
+    char* msg;
+    if (result == Ok)
     {
+        did_fail = 0;
+        msg = "Ok";
+    } else if (result == EqualityError)
+    {
+        msg = "FAILED with an EqualityError";
+    } else {
         msg = "FAILED with unknown error";
     }
     fprintf(stderr, "--- %-30s: %s\n", name, msg);
@@ -150,7 +186,7 @@ int test_sparse_observable()
     num_failed += run("test_num_terms", test_num_terms());
     num_failed += run("test_num_qubits", test_num_qubits());
 
-    fprintf(stderr, "=== Number of failed subtests: %i", num_failed);
+    fprintf(stderr, "=== Number of failed subtests: %i\n", num_failed);
 
     return num_failed;
 }
